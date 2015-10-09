488411251add9d7946302f1e86fe00effb74750a
zhaoqing <qing.zhao@cgtz.com>
2015-10-09 16:40:09 +0800

    verson1.1

--- protected/config/develop.php
+++ protected/config/develop.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'My Web Application',
+    'name'=>'草根投资',
     'defaultController'=>'index',
 
     // preloading 'log' component
