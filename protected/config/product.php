231939144aae99d4d250a78fff983eeb590c8f4c
zhaoqing <qing.zhao@cgtz.com>
2015-11-09 10:21:31 +0800

    admin

--- protected/config/product.php
+++ protected/config/product.php
@@ -7,10 +7,8 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name' => '优时',
-    'language' => 'zh_cn',
-    'charset' => 'utf-8',
-    'theme' => 'classic',
+    'name'=>'优时',
+    //'defaultController'=>'index',
 
     // preloading 'log' component
     'preload'=>array('log'),
@@ -21,125 +19,68 @@ return array(
         'application.components.*',
     ),
 
-    // => array('MyEventHandler', 'BeginRequestMethod'),
-    //'onEndRequest' => array('MyEventHandler', 'EndRequestMethod'),
-    'modules' => array(
+    'modules'=>array(
         // uncomment the following to enable the Gii tool
-
-        'gii' => array(
-            'class' => 'system.gii.GiiModule',
-            'password' => '123456',
+        /*
+        'gii'=>array(
+            'class'=>'system.gii.GiiModule',
+            'password'=>'Enter Your Password Here',
             // If removed, Gii defaults to localhost only. Edit carefully to taste.
-            'ipFilters' => array('127.0.0.1', '::1'),
-//            'newFileMode' => 0666,
-//            'newDirMode' => 0777,
+            'ipFilters'=>array('127.0.0.1','::1'),
         ),
-
+        */
     ),
 
     // application components
-    'components' => array(
-        'user' => array(
+    'components'=>array(
+
+        'user'=>array(
             // enable cookie-based authentication
-            //'class' => 'RWebUser',
-            'allowAutoLogin' => true,
-            'loginUrl' => array('site/login'),
+            'allowAutoLogin'=>true,
         ),
 
         // uncomment the following to enable URLs in path-format
-        'urlManager' => array(
-            'urlFormat' => 'path',
-            'showScriptName' => false,
-            'rules' => array(
-                // '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+
+        'urlManager'=>array(
+            'urlFormat'=>'path',
+            'rules'=>array(
+                '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+                '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
             ),
         ),
 
-        /* 'db'=>array(
-            'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-        ), */
-        // uncomment the following to use a MySQL database
 
-        /*'db'=>array(
-            'connectionString' => 'mysql:host=127.0.0.1;port=3306;dbname=urtimer',
-            'emulatePrepare' => true,
-            'username' => 'root',
-            'password' => 'urtimerqwerty',
-            'charset' => 'utf8',
-            'tablePrefix' => 't_',
-        ),*/
+        // database settings are configured in database.php
+        'db'=>require(dirname(__FILE__).'/database.php'),
 
-        'cache_file' => array(
-            'class' => 'CFileCache',
-        ),
-
-        'cache' => array(
-            'class' => 'ext.redis.CRedisCache',
-            'keyPrefix' => false,
-            'hashKey' => false,
-            //'serializer'=>false,
-            'servers' => array(
-                array(
-                    'host' => '127.0.0.1',
-                    'port' => 6379,
-                   // 'password' => '00000000',
-                ),
-            ),
-        ),
-
-        'errorHandler' => array(
+        'errorHandler'=>array(
             // use 'site/error' action to display errors
-            'errorAction' => 'site/error',
+            'errorAction'=>'site/error',
         ),
-        'log' => array(
-            'class' => 'CLogRouter',
-            // 'autoDump'=>false,
-            'routes' => array(
-                array(
-                    'class' => 'CFileLogRoute',
-                    'enabled' => true,
-                    'categories' => 'system.db.CDbCommand',
-                    'levels' => 'trace,error,warning',
-                    'filter' => array(
-                        'class' => 'CLogFilter',
-                        'logUser' => true,
-                        'prefixUser' => true,
-                    ),
-                    'logFile' => 'db.log',
-                ),
 
+        'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
                 array(
-                    'class' => 'CFileLogRoute',
-                    'enabled' => true,
-                    'levels' => 'error,warning',
-                    'filter' => array(
-                        'class' => 'CLogFilter',
-                        'logUser' => true,
-                        'prefixUser' => true,
-                    ),
+                    'class'=>'CFileLogRoute',
+                    'levels'=>'error, warning',
                 ),
+                // uncomment the following to show log messages on web pages
+                /*
                 array(
-                    'class' => 'CWebLogRoute',
-                    'enabled' => true,
-                    'categories' => 'system.db.*',
-                    'levels' => 'trace,error,warning',
-                    'showInFireBug' => true,
+                    'class'=>'CWebLogRoute',
                 ),
-
+                */
             ),
         ),
+
     ),
 
     // application-level parameters that can be accessed
     // using Yii::app()->params['paramName']
-    'params' => array(
+    'params'=>array(
         // this is used in contact page
         'adminEmail'=>'webmaster@example.com',
-        'up_path' => '/data/uploadfiles',
-        'up_domain' => 'http://img.op.admin.cgtz.com',
-        'project_preview_url'=>'http://sun.dev.cgtz.com/',
     ),
 );
-return CMap::mergeArray($config, require(dirname(__FILE__) . '/params.php'));
