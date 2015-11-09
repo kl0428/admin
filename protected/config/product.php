4490bc6d440ce1089823db371f9e9ba7e3ce8e96
zhaoqing <qing.zhao@cgtz.com>
2015-11-09 11:06:31 +0800

    admin

--- protected/config/product.php
+++ protected/config/product.php
@@ -73,13 +73,13 @@ return array(
             //'keyPrefix' => false,
            // 'hashKey' => false,
             //'serializer'=>false,
-            'servers' => array(
+            /*'servers' => array(
                 array(
                     'host' => '127.0.0.1',
                     'port' => 6379,
                     //'password' => 'urtimerqwerty',
                 ),
-            ),
+            ),*/
         ),
 
         'errorHandler'=>array(
