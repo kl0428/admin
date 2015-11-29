613e4f0c88ee939b1188173176439caa979f01d7
zhaoqing <qing.zhao@cgtz.com>
2015-11-29 17:59:36 +0800

    admin

--- protected/config/product.php
+++ protected/config/product.php
@@ -18,7 +18,7 @@ return array(
     'import'=>array(
         'application.models.*',
         'application.components.*',
-        'application.extensions.redis.*',
+        'application.extensions.*',
     ),
 
     'modules'=>array(
@@ -110,5 +110,50 @@ return array(
     'params'=>array(
         // this is used in contact page
         'adminEmail'=>'webmaster@example.com',
+        'dir'=>dirname(__FILE__),
+        'app'	=>array(
+            'MobileApiKey'=>'1234567',
+            'MobileApiValidtime'=>10*60,
+        ),
+        'chuanglan'=>array(
+            'api_send_url'=>'http://222.73.117.158/msg/HttpBatchSendSM',//创蓝发送短信接口URL, 如无必要，该参数可不用修改
+            'api_balance_query_url'=>'http://222.73.117.158/msg/QueryBalance',//创蓝短信余额查询接口URL, 如无必要，该参数可不用修改
+            'api_account'=>'xxxx',//创蓝账号 替换成你自己的账号
+            'api_password'=>'xxxx',//创蓝密码，以数字和字母组成的32位字符
+        ),
+        'cards' =>array(
+            0=>array('key'=>0,'name'=>'体验通卡'),
+            1=>array('key'=>1,'name'=>'通卡'),
+        ),
+        'imageUrl'=>'http://admin.cgtz.com',
+        'UpYun' => array(
+            'visitUrl' => 'https://cgtzimage.b0.upaiyun.com',//images
+            'httpImgUrl' => 'http://cgtzimage.b0.upaiyun.com',//images
+            'cutUrl' => 'http://v0.api.upyun.com',
+            'bucketname' => 'cgtzimage',
+            'username' => 'caogen',
+            'password' => 'caogen123',
+            'bucket_file_name' => 'cgtzfiles',
+            'attachUrl' => 'http://cgtzfiles.b0.upaiyun.com',//files
+            'projectsUrl' => 'https://cgtzimage.b0.upaiyun.com/projectTests/',//images
+        ),
+        'banklist' => array(
+            'CMB' => '招商银行',
+            'ICBC' => '中国工商银行',//ICBC-D
+            'CCB' => '中国建设银行',
+            'BOC' => '中国银行',
+            'ABC' => '中国农业银行',//ABC-D
+            'HSBC' => '交通银行',
+            'SPDB' => '浦发银行',
+            'GDB'=>'广东发展银行',
+            'CITIC' => '中信银行',//CITIC-D
+            'CEB' => '中国光大银行',
+            'CIB'=>'兴业银行',
+            'CMBC' => '中国民生银行',
+            'HXB' => '华夏银行',
+            'SDB' => '平安银行',
+            'PSBC' => '中国邮政储蓄银行',
+            'HZBANK' => '杭州银行',
+        ),
     ),
 );
