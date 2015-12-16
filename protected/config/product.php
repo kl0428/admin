6a77c1a69c64781c299c611d90ff31afca189f69
zhaoqing <qing.zhao@cgtz.com>
2015-12-16 09:26:33 +0800

    zq

--- protected/config/product.php
+++ protected/config/product.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
     'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-    'name'=>'草根',
+    'name'=>'Urtime',
     //'defaultController'=>'index',
     'theme' => 'classic',
 
